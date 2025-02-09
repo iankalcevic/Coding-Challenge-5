3b534119497974658ce6e3215da91cee820b6e78
iankalcevic <iankalcevic@usf.edu>
2025-02-09 17:01:44 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+//Task 1 - Object Properties
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+}; //Declare an object with three properties
+console.log('Name: ${customer.name}'); //Log customer name
+console.log('Age: ${customer.age}'); //Log customer age
+console.log('Email: ${customer.name}'); //Log customer age
